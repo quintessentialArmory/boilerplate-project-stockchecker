bcedcce53288307b7c4d9f6975d0289ec7b29589
Lucas Montenegro <lucasok@protonmail.com>
2018-07-25 17:07:46 -0300

    added server and index.html

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var express     = require('express');
 var bodyParser  = require('body-parser');
 var expect      = require('chai').expect;
 var cors        = require('cors');
+var helmet      = require('helmet');
 
 var apiRoutes         = require('./routes/api.js');
 var fccTestingRoutes  = require('./routes/fcctesting.js');
@@ -11,13 +12,34 @@ var runner            = require('./test-runner');
 
 var app = express();
 
-app.use('/public', express.static(process.cwd() + '/public'));
-
-app.use(cors({origin: '*'})); //For FCC testing purposes only
+// Set the content security policies to only allow loading of scripts and css from your server.
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+    scriptSrc: ["'self'", "'unsafe-inline'", "https://code.jquery.com/jquery-2.2.1.min.js"],
+    styleSrc: ["'self'", "'unsafe-inline'"],
+  }
+}));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+if (process.env.ENABLE_DEBUGGING == 't') {
+  let count = 0;
+  app.use(function (req, res, next) {
+    count++;
+    let str = count + ' ' + req.method + ' ' + req.url;
+    const t = Date.now();
+    console.log('\nNew request:\n' + str);
+    console.log(req.body);
+    res.on('finish', () => console.log(`\nRequest ended:\n${Date.now() - t}ms ${str}`));
+    next();
+  });
+}
+
+
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.use(cors({origin: '*'})); //For FCC testing purposes only
 //Index page (static HTML)
 app.route('/')
   .get(function (req, res) {
@@ -28,30 +50,31 @@ app.route('/')
 fccTestingRoutes(app);
 
 //Routing for API 
-apiRoutes(app);  
-    
-//404 Not Found Middleware
-app.use(function(req, res, next) {
-  res.status(404)
-    .type('text')
-    .send('Not Found');
-});
+apiRoutes(app, () => {
 
-//Start our server and tests!
-app.listen(process.env.PORT || 3000, function () {
-  console.log("Listening on port " + process.env.PORT);
-  if(process.env.NODE_ENV==='test') {
-    console.log('Running Tests...');
-    setTimeout(function () {
-      try {
-        runner.run();
-      } catch(e) {
-        var error = e;
-          console.log('Tests are not valid:');
-          console.log(error);
-      }
-    }, 3500);
-  }
+  //404 Not Found Middleware
+  app.use(function(req, res, next) {
+    res.status(404)
+      .type('text')
+      .send('Not Found');
+  });
+
+  //Start our server and tests!
+  app.listen(process.env.PORT || 3000, function () {
+    console.log("Listening on port " + process.env.PORT);
+    if(process.env.NODE_ENV==='test') {
+      console.log('Running Tests...');
+      setTimeout(function () {
+        try {
+          runner.run();
+        } catch(e) {
+          var error = e;
+            console.log('Tests are not valid:');
+            console.log(error);
+        }
+      }, 3500);
+    }
+  });
 });
 
 module.exports = app; //for testing
